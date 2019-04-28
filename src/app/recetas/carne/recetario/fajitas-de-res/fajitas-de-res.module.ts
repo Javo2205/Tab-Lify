75bf2a18fbf00bbae4ba28e99221b83d0348f555
unknown <javo.delara@gmail.com>
2019-04-28 12:32:49 -0500

    se agrega la info de la receta

--- src/app/recetas/carne/recetario/fajitas-de-res/fajitas-de-res.module.ts
+++ src/app/recetas/carne/recetario/fajitas-de-res/fajitas-de-res.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { FajitasDeResPage } from './fajitas-de-res.page';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    
   ],
   declarations: [FajitasDeResPage]
 })
