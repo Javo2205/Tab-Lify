554c3b54abcf08059b021ad978ce2349224ac24a
Javo2205 <javo.delara@gmail.ciom>
2019-04-27 23:28:31 -0500

    primer commit con el proyecto
    aun no se agregar las recetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'carne', loadChildren: './recetas/carne/carne.module#CarnePageModule' },
+  { path: 'pollo', loadChildren: './recetas/pollo/pollo.module#PolloPageModule' },
+  { path: 'pescado', loadChildren: './recetas/pescado/pescado.module#PescadoPageModule' },
+  
 ];
 @NgModule({
   imports: [
---
+++ src/app/recetas/carne/carne.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CarnePage } from './carne.page';
+import { ComponentsModule } from '../../components/components.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CarnePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    ComponentsModule
+  ],
+  declarations: [CarnePage]
+})
+export class CarnePageModule {}
