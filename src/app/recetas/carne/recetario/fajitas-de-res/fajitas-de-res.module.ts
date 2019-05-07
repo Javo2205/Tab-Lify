c6f62a654e514688992d5d1f3bdac98f42334b53
Javo2205 <javo.delara@gmail.ciom>
2019-05-07 01:48:57 -0500

    se agregaron las img a cada receta
    falta agregar las demas recetas y listo!

--- src/app/recetas/carne/recetario/fajitas-de-res/fajitas-de-res.module.ts
+++ src/app/recetas/carne/recetario/fajitas-de-res/fajitas-de-res.module.ts
@@ -6,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { FajitasDeResPage } from './fajitas-de-res.page';
+import { ComponentsModule } from 'src/app/components/components.module';
+
 
 
 const routes: Routes = [
@@ -21,7 +23,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-    
+    ComponentsModule
   ],
   declarations: [FajitasDeResPage]
 })
